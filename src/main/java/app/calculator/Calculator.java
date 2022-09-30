73d9b458c2d1a719d19f9eae3b691b625cc444c8
Alejandro Costa <alejandroesfc@gmail.com>
2022-09-30 15:44:58 -0300

    implements subtraction

--- src/main/java/app/calculator/Calculator.java
+++ src/main/java/app/calculator/Calculator.java
@@ -11,4 +11,12 @@ public class Calculator {
     public double add(double firstNumber, double secondNumber) {
         return firstNumber + secondNumber;
     }
+
+    public double subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    public double subtract(double firstNumber, double secondNumber) {
+        return firstNumber - secondNumber;
+    }
 }
--- src/test/java/app/calculator/CalculatorTest.java
+++ src/test/java/app/calculator/CalculatorTest.java
@@ -34,4 +34,22 @@ public class CalculatorTest {
         double expected = 1.5;
         assertEquals(expected, calculator.add(1, 0.5), TOLERANCE);
     }
+
+    @Test
+    public void testGivenTwoIntegersThenSubtractReturnsTheSubtraction() {
+        double expected = 5d;
+        assertEquals(expected, calculator.subtract(7, 2), TOLERANCE);
+    }
+
+    @Test
+    public void testGivenTwoDoublesThenSubtractReturnsTheSubtraction() {
+        double expected = 5.1;
+        assertEquals(expected, calculator.subtract(6.9, 1.8), TOLERANCE);
+    }
+
+    @Test
+    public void testGivenAnIntegerAndADoubleThenSubtractReturnsTheSubtraction() {
+        double expected = 5.1;
+        assertEquals(expected, calculator.subtract(7, 1.9), TOLERANCE);
+    }
 }
