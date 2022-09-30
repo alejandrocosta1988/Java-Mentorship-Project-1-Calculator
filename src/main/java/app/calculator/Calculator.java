53ed6c3a7c08746bbe2801ae566a893c9435a709
Alejandro Costa <alejandroesfc@gmail.com>
2022-09-30 15:35:50 -0300

    implements addition

--- src/main/java/app/calculator/Calculator.java
+++ src/main/java/app/calculator/Calculator.java
@@ -1,2 +1,14 @@
-package app.calculator;public class Calculator {
+package app.calculator;
+
+import java.math.BigDecimal;
+
+public class Calculator {
+
+    public double add(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    public double add(double firstNumber, double secondNumber) {
+        return firstNumber + secondNumber;
+    }
 }
--- src/test/java/app/calculator/CalculatorTest.java
+++ src/test/java/app/calculator/CalculatorTest.java
@@ -1,2 +1,37 @@
-package app.calculator;public class CalculatorTest {
+package app.calculator;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+    private final double TOLERANCE = 0.005;
+
+    @Before
+    public void createCalculator() {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void testGivenTwoIntegersThenAddReturnsTheSum() {
+        double expected = 4d;
+        assertEquals(expected, calculator.add(2, 2), TOLERANCE);
+    }
+
+    @Test
+    public void testGivenTwoDoublesThenAddReturnsTheSum() {
+        double expected = 6.43;
+        assertEquals(expected, calculator.add(5.41, 1.02), TOLERANCE);
+    }
+
+    @Test
+    public void testGivenAnIntegerAndADoubleThenAddReturnsTheSum() {
+        double expected = 1.5;
+        assertEquals(expected, calculator.add(1, 0.5), TOLERANCE);
+    }
 }
