694887f971458685bf878cfbcb1e64e135978cbd
Alejandro Costa <alejandroesfc@gmail.com>
2022-09-30 15:46:05 -0300

    removes unnecessary int int method overloads

--- src/main/java/app/calculator/Calculator.java
+++ src/main/java/app/calculator/Calculator.java
@@ -4,16 +4,8 @@ import java.math.BigDecimal;
 
 public class Calculator {
 
-    public double add(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
-    }
-
     public double add(double firstNumber, double secondNumber) {
         return firstNumber + secondNumber;
-    }
-
-    public double subtract(int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
     }
 
     public double subtract(double firstNumber, double secondNumber) {
