05de90381154a031cef5840c2b7729dfea2e597f
Alejandro Costa <alejandroesfc@gmail.com>
2022-10-03 11:37:28 -0300

    adds circles to suite

--- src/test/java/app/AllTests.java
+++ src/test/java/app/AllTests.java
@@ -4,10 +4,14 @@ import app.calculator.AreaCalculatorTest;
 import app.calculator.BasicOperationTest;
 import app.calculator.BmiCalculatorTest;
 import app.calculator.StandardCalculatorTest;
+import app.shapes.CircleTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ BasicOperationTest.class, StandardCalculatorTest.class, BmiCalculatorTest.class, AreaCalculatorTest.class })
+@Suite.SuiteClasses({
+        BasicOperationTest.class, StandardCalculatorTest.class, BmiCalculatorTest.class,
+        AreaCalculatorTest.class,
+        CircleTest.class })
 public class AllTests {
 }
