0abd9ca418d85b39ec83ad4aec599e858bede183
Alejandro Costa <alejandroesfc@gmail.com>
2022-10-03 11:45:10 -0300

    calculates the area of a circle

--- src/main/java/app/calculator/AreaCalculator.java
+++ src/main/java/app/calculator/AreaCalculator.java
@@ -1,4 +1,24 @@
 package app.calculator;
 
+import app.shapes.Circle;
+
 public class AreaCalculator extends Calculator {
+
+    private Circle shape;
+
+    public void changeShape(Circle shape) {
+        this.shape = shape;
+    }
+
+    public Circle getShape() {
+        return shape;
+    }
+
+    @Override
+    public double getResult() {
+        if (shape == null) {
+            return 0;
+        }
+        return shape.calculateArea();
+    }
 }
--- src/test/java/app/calculator/AreaCalculatorTest.java
+++ src/test/java/app/calculator/AreaCalculatorTest.java
@@ -1,13 +1,16 @@
 package app.calculator;
 
+import app.shapes.Circle;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class AreaCalculatorTest {
 
     private AreaCalculator calculator;
+    private Circle circle;
     private final double TOLERANCE = 0.05;
 
     @Before
@@ -15,9 +18,26 @@ public class AreaCalculatorTest {
         calculator = new AreaCalculator();
     }
 
+    @Before
+    public void createShapes() {
+        circle = new Circle(2.5);
+    }
+
     @Test
     public void testAreaCalculatorIsCreatedWithZeroResult() {
         assertEquals(0d, calculator.getResult(), TOLERANCE);
+    }
+
+    @Test
+    public void testShapesCanBeChangedInTheCalculator() {
+        assertNull(calculator.getShape());
+        calculator.changeShape(circle);
+        assertEquals(circle, calculator.getShape());
+    }
+    @Test
+    public void testGivenARadiusThenCalculateAreaCalculatesTheAreaOfTheCircle() {
+        calculator.changeShape(circle);
+        assertEquals(19.635, calculator.getResult(), TOLERANCE);
     }
 
 }
