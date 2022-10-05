988d93d763a8460318629809ae3d3aa6b57fc2a0
Alejandro Costa <alejandroesfc@gmail.com>
2022-10-05 17:28:40 -0300

    implements printMainMenu and printPrompt

--- src/main/java/app/messages/Menu.java
+++ src/main/java/app/messages/Menu.java
@@ -4,13 +4,18 @@ import app.util.MessageUtil;
 
 public class Menu {
 
-    public static final String MAIN_MENU = "Choose a calculator: " + MessageUtil.LINE_BREAK +
+    public final String MAIN_MENU = "Choose a calculator: " + MessageUtil.LINE_BREAK +
             "1 - Standard calculator" + MessageUtil.LINE_BREAK +
             "2 - Body Mass Index (BMI) calculator" + MessageUtil.LINE_BREAK +
             "3 - Area calculator";
 
-    public static String printPrompt() {
-        return "> ";
+    public final String PROMPT = "> ";
+
+    public String printPrompt() {
+        return PROMPT;
     }
 
+    public String printMainMenu() {
+        return MAIN_MENU;
+    }
 }
--- src/test/java/app/messages/MenuTest.java
+++ src/test/java/app/messages/MenuTest.java
@@ -1,15 +1,27 @@
 package app.messages;
 
+import app.util.MessageUtil;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class MenuTest {
 
+    private Menu menu = new Menu();
     private String prompt = "> ";
 
     @Test
-    public void testPrintsPromptToUser() {
-        assertEquals(prompt, Menu.printPrompt());
+    public void testReturnsPromptToUser() {
+        assertEquals(prompt, menu.printPrompt());
     }
+
+    @Test
+    public void testReturnsMainMenu() {
+        String mainMenu = "Choose a calculator: " + MessageUtil.LINE_BREAK +
+                "1 - Standard calculator" + MessageUtil.LINE_BREAK +
+                "2 - Body Mass Index (BMI) calculator" + MessageUtil.LINE_BREAK +
+                "3 - Area calculator";
+        assertEquals(mainMenu, menu.printMainMenu());
+    }
+
 }
