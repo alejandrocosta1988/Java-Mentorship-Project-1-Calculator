47bd5c0431164c10261a631f9afdce627af12815
Alejandro Costa <alejandroesfc@gmail.com>
2022-10-04 23:47:41 -0300

    adds menu tests to the suite

--- src/test/java/app/AllTests.java
+++ src/test/java/app/AllTests.java
@@ -4,6 +4,7 @@ import app.calculator.AreaCalculatorTest;
 import app.calculator.BasicOperationTest;
 import app.calculator.BmiCalculatorTest;
 import app.calculator.StandardCalculatorTest;
+import app.messages.MenuTest;
 import app.shapes.CircleTest;
 import app.shapes.RectangleTest;
 import app.shapes.SquareTest;
@@ -16,7 +17,8 @@ import org.junit.runners.Suite;
         BasicOperationTest.class, StandardCalculatorTest.class, BmiCalculatorTest.class,
         AreaCalculatorTest.class,
         CircleTest.class, SquareTest.class, RectangleTest.class,
-        MessageUtilTest.class
+        MessageUtilTest.class,
+        MenuTest.class
 })
 public class AllTests {
 }
