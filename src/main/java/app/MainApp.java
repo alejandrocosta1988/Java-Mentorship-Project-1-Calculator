2ae5a99483fc9cd3dffaa592c7e1ecdbddfe20db
Alejandro Costa <alejandroesfc@gmail.com>
2022-10-04 23:48:40 -0300

    prints the main menu and a user prompt. captures user option

--- src/main/java/app/MainApp.java
+++ src/main/java/app/MainApp.java
@@ -1,2 +1,20 @@
-package app;public class MainApp {
+package app;
+
+import app.messages.Menu;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(Menu.MAIN_MENU);
+        System.out.print(Menu.printPrompt());
+        Scanner in = new Scanner(System.in);
+        int userOption = in.nextInt();
+
+        System.out.println(userOption);
+    }
+
 }
